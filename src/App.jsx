04a9f4ba4bd890413d2862def3c24fdda010e9ac
Muhammad Apu <apu16-334@diu.edu.bd>
2026-08-29 23:21:21 +0600

    Set up issue tracker frontend react app with axios client

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import axiosClient from "./api/axiosClient"
+import { useEffect } from "react"
+
+function App() {
+
+    
+
+    return (
+        <>
+            <h2>Hello World.........!</h2>
+
+
+        </>
+    )
+}
+
+export default App
