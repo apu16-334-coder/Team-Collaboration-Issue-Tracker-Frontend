848863e881f27749c3ad83796202752f28d342a0
Muhammad Apu <apu16-334@diu.edu.bd>
2026-09-05 21:07:57 +0600

    Implemented get all project list

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,7 +1,50 @@
+import { useEffect, useState } from "react";
+import axiosClient from "../api/axiosClient";
+import { Link } from "react-router-dom";
+
+
 function Project() {
+    const [projects, setProjects] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchProjects() {
+            try {
+                setIsLoading(true);
+                setError(null);
+
+                const response = await axiosClient.get('/projects');
+
+                setProjects(response.data.data)
+
+
+            } catch (err) {
+                setError(err.response?.data?.message || 'Fetching projects failed')
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+        fetchProjects();
+    }, [])
+
+    if (isLoading) return <p>Loading projects...</p>
+    if (error) return <p style={{ color: 'red' }}>{error}</p>
+
     return (
         <>
-            <h2>Project Page</h2>
+            <div>
+                <h1>Projects</h1>
+                {projects.length === 0 && <p>No projects yet.</p>}
+                <ul>
+                    {projects.map(project => (
+                        <li key={project.id}>
+                            <Link to={`/projects/${project.id}`}>{project.title}</Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     )
 }
